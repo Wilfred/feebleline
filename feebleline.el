e1a2a4c802b33e8812200e7d7cd27c7ea9c04110
Wilfred Hughes <me@wilfred.me.uk>
2018-03-17 16:40:26 +0000

    Add autoload cookie
    Otherwise users are forced to require the file to get started.

--- feebleline.el
+++ feebleline.el
@@ -14,7 +14,6 @@
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -121,6 +120,8 @@ sent to `add-text-properties'.")
 
 (defvar feebleline/timer)
 (defvar feebleline/mode-line-format-previous)
+
+;;;###autoload
 (define-minor-mode feebleline-mode
   "Replace modeline with a slimmer proxy."
   :require 'feebleline
